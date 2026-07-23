bed2eb72fb9a3baa2bc9e3bf164bc053ba41f0e4
Aayush <ikps562@gmail.com>
2026-07-23 19:05:42 +0530

    WAF of add and multiply

---
+++ lab1/fun.js
@@ -0,0 +1,9 @@
+// WAF where to take a digit ( 0-9) and and return its spelling or words
+
+function words(a) {
+    if (a=0) {
+        console.log("Zero");    
+    }
+}
+
+console.log(words(1))
---
+++ lab1/hello.js
@@ -0,0 +1,14 @@
+console.log(" I Am Aayush Kumar")
+// write a function to take 2 number and retturn their sum
+
+function sum(a,b) {
+    return a+b;
+    
+}
+
+const mul =(a,b)=>{
+    return a*b;
+}
+console.log( sum(11,11));
+console.log(sum(34,35));
+console.log(mul(45, 1005));
