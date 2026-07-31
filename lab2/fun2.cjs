a63e75ca1dfeb477a33d74f5abc4a402d6c0fdb2
Aayush <ikps562@gmail.com>
2026-07-31 14:31:19 +0530

    asyncronous js explain

---
+++ lab2/fun2.cjs
@@ -0,0 +1,23 @@
+// WAF where to take a digit ( 0-9) and and return its spelling or words
+
+const f1 = () => {
+  console.log("F1");
+};
+const f2 = () => {
+  console.log("F2");
+};
+const f3 = () => {
+  console.log("F3");
+};
+
+function main() {
+  console.log("main");
+
+  setTimeout(f1, 0);
+  //setInterval(f1,1000);
+  setImmediate(f2);
+
+  f3();
+  console.log("end👌👌");
+}
+main();
