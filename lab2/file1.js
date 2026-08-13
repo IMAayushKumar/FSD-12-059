a9d750d3c99b43b2890764c18124cfe98b3e61c0
Aayush <ikps562@gmail.com>
2026-08-13 11:05:27 +0530

    append file and read data

--- lab2/file1.js
+++ lab2/file1.js
@@ -1,4 +1,11 @@
-import { writeFile,appendFile } from "fs/promises";
+// import { writeFile,appendFile } from "fs/promises";
+import { writeFile, appendFile,readFile } from "fs/promises";
 
 // await writeFile("hello.txt","Hello JS");
-await appendFile("hello.txt","\nFS is much easy than other.")
+await appendFile("hello.txt","\n JS is much easy than othe r. ❤️❤️")
+// asynchronous type of file 
+await appendFile("hello.txt", "\naayush kumar ");
+const content=await readFile("hello.txt","utf-8")
+console.log(content);
+
+
